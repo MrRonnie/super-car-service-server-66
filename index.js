6910f39c8c932a3e343a98895a9a69c294ff18ee
MrRonnie <ronymaza55@gmail.com>
2022-04-25 16:59:23 +0600

    env delete user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const res = require("express/lib/response");
+const req = require("express/lib/request");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -36,6 +37,21 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
+    });
+
+    // POST
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // DELETE
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
   }
